bb0690ab8a2048a6138b2c9bfd541eecc35e33cb
Christian <christian.garba@gmail.com>
2020-12-27 12:10:24 -0700

    done

---
+++ 7Kyu/Consecutive-Ducks/Solution.js
@@ -0,0 +1,13 @@
+function consecutiveDucks(num) {
+    return !Number.isInteger( Math.log2(num) );
+  }
+
+//   OR
+
+function consecutiveDucks(num) {
+    while(num>2){
+      if(num%2!=0)return true
+      num=num/2;
+    }
+    return false
+  }
