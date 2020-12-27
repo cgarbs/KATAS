f55fa41ce955c9f80c4dca8fd4d08f669fa5221d
Christian <christian.garba@gmail.com>
2020-12-27 11:08:35 -0700

    done

---
+++ 7Kyu/How-Many-Days-Are-We-Represented-in-a-Foreign-Country/Solution.js
@@ -0,0 +1,11 @@
+function daysRepresented(trips) {
+    const set = new Set();
+    trips.map(([a, b]) => {
+      for (let i = a; i <= b; ++i) {
+        set.add(i);
+      }
+    });
+    return set.size;
+  }
+
+//   NOT ORIGINAL SOLUTION.
