977cf5fa5755da719e672855a1c8e2bb43fe4d76
Christian <christian.garba@gmail.com>
2020-12-27 19:42:32 -0700

    done

--- 6Kyu/Array.diff/Alternate-Solution.js
+++ 6Kyu/Array.diff/Alternate-Solution.js
@@ -1,3 +1,9 @@
 function array_diff(a, b) {
     return a.filter(e => !b.includes(e));
   }
+
+//   OR
+
+function array_diff(a, b) {
+    return a.filter(function(x) { return b.indexOf(x) == -1; });
+  }
