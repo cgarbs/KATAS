c466e0a66106b2a396a3bcf6fe0feb5a542bc806
Christian <christian.garba@gmail.com>
2021-01-10 23:07:23 -0700

    done

--- 6Kyu/+1Array/Alternate-Solution.js
+++ 6Kyu/+1Array/Alternate-Solution.js
@@ -3,4 +3,17 @@ function upArray(arr) {
     return newArr.toString().split('').map(Number);
   }
 
-//   Simple solution, however Kata does not support BigInt.
+// Simple solution, however Kata does not support BigInt.
+
+
+const BigNumber = require('bignumber.js');
+
+const upArray = arr =>
+  arr.length === 0 || arr.filter(x => x < 0 || x > 9).length > 0
+    ? null
+    : (new BigNumber(arr.join('')).plus(1))
+        .toPrecision()
+        .split('')
+        .map(Number);
+
+// This solution uses a JS library.
