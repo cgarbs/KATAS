7a6b46189de552b80b37e59e12400a78eb7cc6ea
Christian <christian.garba@gmail.com>
2021-01-18 13:35:48 -0700

    done

---
+++ 6Kyu/Multiplication-Table/Solution.js
@@ -0,0 +1,9 @@
+multiplicationTable = function(size) {
+    let array = [[]];
+    for(let i = 1; i <= size; i++) {
+      array[0].push(i);
+    }
+    for(let n = 2; n <= size; n++) {
+      array.push(array[0].map(e => e * n))
+    } return array;
+  }
