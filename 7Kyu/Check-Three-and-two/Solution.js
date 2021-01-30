89d14b0338075c06fb9405c3b74eef6715bf0f0b
Christian <christian.garba@gmail.com>
2021-01-30 11:31:08 -0700

    done

---
+++ 7Kyu/Check-Three-and-two/Solution.js
@@ -0,0 +1,25 @@
+function checkThreeAndTwo(array) {
+    let aCount = 0;
+    let bCount = 0;
+    let cCount = 0;
+  
+    for(e of array) {
+      if(e === "a") {
+        aCount++
+      }
+    }
+     for(e of array) {
+      if(e === "b") {
+        bCount++
+      }
+    }
+     for(e of array) {
+      if(e === "c") {
+        cCount++
+      }
+    } if((aCount === 2 || bCount === 2 || cCount === 2) && (aCount === 3 || bCount == 3 || cCount === 3)) {
+      return true
+    } else {
+      return false;
+    }
+  }
