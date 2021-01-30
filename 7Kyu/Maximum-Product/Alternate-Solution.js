57bd168c63e1e13866163990de9619fb588d70fe
Christian <christian.garba@gmail.com>
2021-01-30 14:09:29 -0700

    done

---
+++ 7Kyu/Maximum-Product/Alternate-Solution.js
@@ -0,0 +1,15 @@
+function adjacentElementsProduct(array) {
+    let newArr = []
+    for(i=0; i < array.length-1; i++){
+      newArr.push(array[i]*array[i+1])
+    }  
+    return Math.max(...newArr)
+  }
+
+// OR
+
+function adjacentElementsProduct(a) {
+    return Math.max(...a.map((x,i)=>x*a[i+1]).slice(0,-1))
+  }
+
+//   (One liner)
