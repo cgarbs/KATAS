10a912a01e835d4391eb131b2039fa4ee5c5e077
Christian <christian.garba@gmail.com>
2021-02-24 23:48:38 -0700

    done

--- 5Kyu/Sum-of-Pairs/Solution.js
+++ 5Kyu/Sum-of-Pairs/Solution.js
@@ -1,3 +1,21 @@
-function sumPairs(ints, s) {
-  return [0, 0] || undefined;
+// function sumPairs(ints, s) {
+//   return [0, 0] || undefined;
+// }
+
+sumPairs = (ints, s) => {
+  sum = [];
+  for(let i = 0; i < ints.length; i++) {
+    for(let j = 0; j < ints.length; j++) {
+      if(ints[i] + ints[j] === s) {
+        sum.push(ints[i],ints[j])
+        return sum;
+      }
+    }
+  }
 }
+
+
+let test = [10, 5, 5, 2, 6]
+sumPairs(test, 10);
+
+//Will return correct integers but not the correct solution.
