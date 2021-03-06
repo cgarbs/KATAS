4e577c0861a123d388d3b8f89ee0d8e4eda036ab
Christian <christian.garba@gmail.com>
2021-03-05 23:48:29 -0700

    done

--- 6Kyu/Multiplication-Table/Solution.js
+++ 6Kyu/Multiplication-Table/Solution.js
@@ -1,9 +1,12 @@
-multiplicationTable = function(size) {
-    let array = [[]];
-    for(let i = 1; i <= size; i++) {
-      array[0].push(i);
-    }
-    for(let n = 2; n <= size; n++) {
-      array.push(array[0].map(e => e * n))
-    } return array;
+multiplicationTable = function (size) {
+  let array = [[]];
+  for (let i = 1; i <= size; i++) {
+    array[0].push(i);
   }
+  for (let n = 2; n <= size; n++) {
+    array.push(array[0].map((e) => e * n));
+  }
+  return array;
+};
+
+// Likely not the most efficient, but possibly the simplest solution.
