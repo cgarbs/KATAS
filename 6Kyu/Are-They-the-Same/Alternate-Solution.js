1595d4d0ff921dbe2a56d191d10a8c35dc668fb2
Christian <christian.garba@gmail.com>
2021-03-07 22:05:05 -0700

    done

--- 6Kyu/Are-They-the-Same/Alternate-Solution.js
+++ 6Kyu/Are-They-the-Same/Alternate-Solution.js
@@ -1,11 +1,23 @@
 function comp(array1, array2) {
-    if(array1 == null || array2 == null) return false;
-    array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
-    return array1.map(v => v * v).every((v, i) => v == array2[i]);
-  }
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.map((v) => v * v).every((v, i) => v == array2[i]);
+}
+
+//   Clean
 
 //   OR
 
 function comp(a, b) {
-    return !!a && !!b && a.map(x => x*x).sort().join() == b.sort().join();
-  }
+  return (
+    !!a &&
+    !!b &&
+    a
+      .map((x) => x * x)
+      .sort()
+      .join() == b.sort().join()
+  );
+}
+
+//  Readable one-liner - impressive.
