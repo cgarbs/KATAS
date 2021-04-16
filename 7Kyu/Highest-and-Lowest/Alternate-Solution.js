4c64038e81038082c024e0aac1521daf4e536dea
Christian <christian.garba@gmail.com>
2021-04-16 00:06:48 -0700

    done

---
+++ 7Kyu/Highest-and-Lowest/Alternate-Solution.js
@@ -0,0 +1,7 @@
+function highAndLow(numbers){
+    numbers = numbers.split(' ').map(Number);
+    return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+  }
+
+// Utilizes .apply
+// Virtually all alternate solutions look like original code.
