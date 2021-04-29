88b549bca82df68786f4066ef5aac7524f6c204e
Christian <christian.garba@gmail.com>
2021-04-28 23:50:55 -0700

    done

--- 5Kyu/Extract-the-Domain-Name-from-a-URL/Solution.js
+++ 5Kyu/Extract-the-Domain-Name-from-a-URL/Solution.js
@@ -1,3 +1,8 @@
 function domainName(url){
     //your code here
   }
+
+// TLD may be followed by multiple directories
+// URL may or may not include HTTP, HTTPS or www
+// Do tests include URLs without a protocol or prefix?
+// Possible solution: parse from TLD backwards.
