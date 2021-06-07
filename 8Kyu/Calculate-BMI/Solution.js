58b11db3a6e5e09dd4b12c391b693857d65290c5
cgarbs <christian.garba@gmail.com>
2021-06-07 00:42:27 -0700

    done

--- 8Kyu/Calculate-BMI/Solution.js
+++ 8Kyu/Calculate-BMI/Solution.js
@@ -1,3 +1,9 @@
 function bmi(weight, height) {
-    return "";
+    const index = weight / (height * height);
+    let result = (
+    index <= 18.5 ? "Underweight" :
+    index <= 25.0 ? "Normal" :
+    index <= 30.0 ? "Overweight" : "Obese"
+    )
+    return result;
   }
