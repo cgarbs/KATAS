f2b5d273b91dab6fadafea65e7aca7fafdcec988
cgarbs <christian.garba@gmail.com>
2021-07-05 12:01:50 -0700

    done

--- 6Kyu/Find-the-Missing-Letter/Solution.js
+++ 6Kyu/Find-the-Missing-Letter/Solution.js
@@ -1,2 +1,5 @@
 // Work in progress.
 // Solution is likely ASCII based.
+// loop through array of letters converted to ASCII characters,
+// compare each following index to the expected ASCII character (i + 1) * (j + 1).
+// return the ASCII character that is not equal to the index being compared, convert back to letter.
