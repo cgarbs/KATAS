d00c456a61408a022cb415b684893c29657b74fc
Christian <christian.garba@gmail.com>
2021-07-21 17:47:15 -0700

    done

--- 5Kyu/Sum-of-Pairs/Solution.js
+++ 5Kyu/Sum-of-Pairs/Solution.js
@@ -1,19 +1,30 @@
-const sumPairs = (ints, s) => {
-  sum = [];
-  for(let i = 0; i < ints.length; i++) {
-    for(let j = 0; j < ints.length; j++) {
-      if(ints[i] + ints[j] === s) {
-        sum.push(ints[i],ints[j])
-        return sum;
+sumPairs = (ints, s) => {
+  array = []
+  sum = []
+  for(let x = 0; array.length < ints.length; x++) {
+    array.push(ints[x])
+    for(let i = 0; i < array.length; i++) {
+      for(let j = i + 1; j < array.length; j++) {
+        if(array[i] + array[j] === s) {
+          sum.push(array[i],array[j])
+          return sum;
+        }
       }
     }
   }
 }
 
-// Will return correct integers but not the correct solution.
-// Must return the pair found first from the LEFT:
-// 10                   // no pair
-// 10, 5                // no pair adding to 10
-// 10, 5, 2             // no pair adding to 10
-// 10, 5, 2, 3          // no pair adding to 10
-// 10, 5, 2, 3, 7       // a pair adding to 10: 3 + 7, done
+// Solution passes all tests but does not complete long list tests
+// (execution time out)
+
+sumPairs = (ints, s) => {
+  let numSet = new Set()
+  numSet.add(ints[0])
+  for (let i = 1; i < ints.length; i++){
+    let pair = s - ints[i]
+    if(numSet.has(pair)) {
+      return [pair, ints[i]]
+    }
+    numSet.add(ints[i])
+  }
+}
