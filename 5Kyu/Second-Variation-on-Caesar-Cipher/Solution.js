b00875d5b7598c6bb26bc3d506e6b91de0a40338
Christian <christian.garba@gmail.com>
2021-07-31 23:56:53 -0700

    done

--- 5Kyu/Second-Variation-on-Caesar-Cipher/Solution.js
+++ 5Kyu/Second-Variation-on-Caesar-Cipher/Solution.js
@@ -1,7 +1,22 @@
 function encodeStr(s, shift) {
-    // your code
-}
+    let first = String.fromCharCode(s.charCodeAt(s[0]) + shift).toLowerCase()
+    let prefix = s[0].toLowerCase().concat(first)
+    let ascii = s.split('').map(x => x.charCodeAt(x) >= 65 && x.charCodeAt(x) <= 90 ? x.charCodeAt(x) + 1 : 
+                                      x.charCodeAt(x) >= 97 && x.charCodeAt(x) <= 122 ? x.charCodeAt(x) + 1 :
+                                      x.charCodeAt(x))
+    let encoded = ascii.map(x => String.fromCharCode(x))
+    let result = prefix.concat(encoded.join(''))
+    let x = Math.ceil(result.length / 5)
+    // let re = new RegExp(/.test/)
+    // console.log(result.match(/.{1,x}/g))
+  }
+  
+  function decode(arr) {
+      // your code
+  }
+  
+  
+  encodeStr("I should have known that you would have a perfect answer for me!!!", 1)
+  //  ["ijJ tipvme ibw", "f lopxo uibu z", "pv xpvme ibwf ", "b qfsgfdu botx", "fs gps nf!!!"]
 
-function decode(arr) {
-    // your code
-}
+  //WORK IN PROGRESS
