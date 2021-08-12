a5001da7efd92e88c4aef697b5ff9afa3b55d825
cgarbs <christian.garba@gmail.com>
2021-08-11 22:04:57 -0700

    done

--- 5Kyu/Formatting-a-number-as-price/Solution.js
+++ 5Kyu/Formatting-a-number-as-price/Solution.js
@@ -15,5 +15,21 @@ var numberToPrice = function(number) {
     return num[0]
   }
   
+  numberToPrice(13253.5123)
+
+  var numberToPrice = function(number) {
+    let num = number.toString().split('.')
+    let count = 0;
+    for(let i = num[0].length; i <= 0; i--) {
+      count++;
+      if(count === 3) {
+        count = 0;
+        //insert comma here?
+      }
+    }
+    num[1].length === 1 ? num[1] = '.' + num[1] + '0' : num[1] = '.' + num[1].slice(0, 2)
+    return num[0]
+  }
+  
   
   numberToPrice(13253.5123)
