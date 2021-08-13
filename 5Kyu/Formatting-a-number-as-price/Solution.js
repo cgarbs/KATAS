7ea3c0b9eebf80af17da63dfbb78b1c89a36f377
cgarbs <christian.garba@gmail.com>
2021-08-12 21:25:12 -0700

    done

--- 5Kyu/Formatting-a-number-as-price/Solution.js
+++ 5Kyu/Formatting-a-number-as-price/Solution.js
@@ -1,11 +1,9 @@
-//WORK IN PROGRESS
 var numberToPrice = function(number) {
-    console.log(number)
-    if(typeof number !== number) NaN;
+    if(typeof number !== 'number') return 'NaN';
     let num = number.toString().split('.')
-    num.length === 1 ? num.push('00') : 
-    num[1].length === 1 ? num[1] = num[1] + '0' : 
-    num[1] = num[1].slice(0, 2);
+      num.length === 1 ? num.push('00') : 
+      num[1].length === 1 ? num[1] = num[1] + '0' : 
+      num[1] = num[1].slice(0, 2);
     let result = Number(num.join('.'))
     return (result).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
   }
