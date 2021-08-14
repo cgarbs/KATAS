5acf8523d6a03f3365e9a12ac361ea09851caf4c
cgarbs <christian.garba@gmail.com>
2021-08-13 19:42:52 -0700

    done

--- 5Kyu/Convert-A-Hex-String-To-RGB/Solution.js
+++ 5Kyu/Convert-A-Hex-String-To-RGB/Solution.js
@@ -1,10 +1,6 @@
-function hexStringToRGB(hexString) {
+const hexStringToRGB = (hexString) => {
     const rgb = hexString.substring(1).match(/.{1,2}/g).map(x => parseInt(x, 16))
-    const result = {
-      r: rgb[0],
-      g: rgb[1],
-      b: rgb[2]
-    }
+    const result = { r: rgb[0], g: rgb[1], b: rgb[2] }
     return result;
   }
   
