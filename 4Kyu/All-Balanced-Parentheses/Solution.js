18d04c3b9aeb86347cc3cdbacc92fe6a7485fa88
cgarbs <christian.garba@gmail.com>
2021-08-14 20:58:17 -0700

    done

--- 4Kyu/All-Balanced-Parentheses/Solution.js
+++ 4Kyu/All-Balanced-Parentheses/Solution.js
@@ -1,8 +1,5 @@
+//WORK IN PROGRESS.
+
 function balancedParens(n) {
     return [];
   }
-
-// Solution can use a stack involving a string containing
-// all possible characters, comparing characters by index
-// (evens to odds), returning boolean if characters are both equal
-// ordered and balanced (identical to onRamp testing).
---
+++ 5Kyu/Gap-in-Primes/Solution.js
@@ -0,0 +1,7 @@
+function gap(g, m, n) {
+    // your code
+}
+
+//g = gap between primce numbers
+//m = starting number
+//n = ending number
