eab3db2b10ade0a45dcfeb9f00f8576b2edbcf9a
cgarbs <christian.garba@gmail.com>
2021-08-15 18:28:11 -0700

    done

--- 5Kyu/Gap-in-Primes/Solution.js
+++ 5Kyu/Gap-in-Primes/Solution.js
@@ -1,7 +1,28 @@
-function gap(g, m, n) {
-    // your code
+function primeNum(x) {
+	if (x === 2) return true;
+	if (x % 2 !== 0) {
+		for (let i = 2; i <= x / 2; i++) {
+			if (x % i === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
 }
 
-//g = gap between primce numbers
-//m = starting number
-//n = ending number
+function gap(g, m, n) {
+	let cache = null;
+	for(let i = m; i < n; i++) {
+		if(primeNum(i)) {
+			if(cache === null) {
+				cache = i;
+			} else if(i - cache === g) {
+				return [cache, i];
+			} else {
+				cache = i;
+			}
+		}
+	}
+	return null;
+}
