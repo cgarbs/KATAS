1078f65ba4197b11722a91db23e46307ecc5013a
cgarbs <christian.garba@gmail.com>
2021-12-07 21:31:00 -0700

    done

--- 7Kyu/Automorphic-Number-Special-Numbers-Series-#6/Solution.js
+++ 7Kyu/Automorphic-Number-Special-Numbers-Series-#6/Solution.js
@@ -1,3 +1,13 @@
 function automorphic(n){
-  //your code here
+  //Silly solution
+  let square = n * n;
+  let length = n.toString().length
+  console.log(n)
+  let num = square.toString().slice(-length);
+  console.log(num)
+  if (n.toString() === square.toString().slice(-length)) {
+      return "Automorphic"
+    } else {
+    return "Not!!"
+  }
 }
