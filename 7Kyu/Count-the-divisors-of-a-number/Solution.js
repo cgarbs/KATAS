2419fdf14b4725a26d429d59b85e832845a9bea4
cgarbs <christian.garba@gmail.com>
2022-06-02 03:07:38 -0700

    done

--- 7Kyu/Count-the-divisors-of-a-number/Solution.js
+++ 7Kyu/Count-the-divisors-of-a-number/Solution.js
@@ -1,3 +1,15 @@
 function getDivisorsCnt(n){
-    // todo
+    let count = 0;
+    for(let i = 0; i <= n; i++) {
+      if(n % i == 0) {
+        count++
+      }
+    } return count
 }
+
+//OR 
+
+const getDivisorsCnt = (n) => {
+    for(count = 0, i = 0; i <= n; i++) n % i === 0 && count++;
+    return count;
+  }
