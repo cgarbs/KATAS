db4edecddd3039baa52bbeb6f15f3d81333cf41c
cgarbs <christian.garba@gmail.com>
2022-06-04 04:13:38 -0700

    done

--- 6Kyu/Pile-of-Towels/Solution.js
+++ 6Kyu/Pile-of-Towels/Solution.js
@@ -1,6 +1,9 @@
-function sortThePile(pileOfTowels, weeklyUsedTowels) {
-  // Your code here
-  return;
+const sortThePile = (pileOfTowels, weeklyUsedTowels) => {
+  for(let i = 0; i < weeklyUsedTowels.length; i++) {
+    let num = pileOfTowels.length - weeklyUsedTowels[i];
+    let x = pileOfTowels.slice(0, num)
+    let y = pileOfTowels.slice(num)
+    pileOfTowels = x.concat(y.sort().reverse())
+  }
+  return pileOfTowels
 }
-
-// Solution coming soon.
