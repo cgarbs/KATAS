5df8e42a7d44348ceff4784bb67d0ae4f515f7ed
cgarbs <christian.garba@gmail.com>
2022-07-20 21:34:13 -0500

    done

--- 6Kyu/Two-Sum/Solution.js
+++ 6Kyu/Two-Sum/Solution.js
@@ -7,3 +7,13 @@ function twoSum(numbers, target) {
       }
     }
   }
+
+const twoSum = (numbers, target) => {
+  for(let i = 0; i < numbers.length; i++) {
+    for(let j = i + 1; j < numbers.length; j++) {
+      if(numbers[i] + numbers[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
