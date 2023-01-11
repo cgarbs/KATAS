9f4bbfc7aec91e95a0dda83ef530c16feabce94c
cgarbs <christian.garba@gmail.com>
2023-01-11 15:26:38 -0600

    done

---
+++ 6Kyu/Kingdoms-Ep1Jousting/Solution.js
@@ -0,0 +1,10 @@
+function joust(listField, vKnightLeft, vKnightRight) {
+    for(let i = 0; i < listField[0].length; i++) {
+      if(listField[0].indexOf('>') >= listField[1].indexOf('<') || (vKnightLeft == 0 && vKnightRight == 0)) {
+        return listField
+      } else {    
+        listField[0] = ' '.repeat(vKnightLeft) + listField[0].slice(0, listField[0].length - vKnightLeft);
+        listField[1] = listField[1].slice(vKnightRight) + ' '.repeat(vKnightRight);
+      }
+    }
+  }  
