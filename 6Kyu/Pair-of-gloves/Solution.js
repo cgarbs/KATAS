10680ad86553d66dc155fbef2641b6b2bd4a98b9
cgarbs <christian.garba@gmail.com>
2023-01-13 06:19:02 -0600

    done

--- 6Kyu/Pair-of-gloves/Solution.js
+++ 6Kyu/Pair-of-gloves/Solution.js
@@ -1,4 +1,11 @@
-function numberOfPairs(gloves)
-{
-  //My hands are freezing
+const numberOfPairs = (gloves) => {
+  let count = {}
+  for(const e of gloves) {
+    count[e] = count[e] ? count[e] + 1 : 1;
+  }
+  return Object.values(count).map((e) => e % 2 === 0 ? e : e - 1 ).reduce((a,b) => a + b) / 2
 }
+
+// OR
+
+numberOfPairs=a=>[...new Set(a)].map(b=>a.join``.split(b).length-1).reduce((a,b)=>a+(b>>1),0)
