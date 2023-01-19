fa925d7195379f57b828ad6d9073b53c45d35225
cgarbs <christian.garba@gmail.com>
2023-01-19 10:59:14 -0600

    done

--- 5Kyu/A-Man-and-his-Umbrellas/Solution.js
+++ 5Kyu/A-Man-and-his-Umbrellas/Solution.js
@@ -1,3 +1,16 @@
-function minUmbrellas(weather) {
-    // TODO
+const minUmbrellas = (weather) => {
+  let umbrellas = 0;
+  let atWork = 0;
+  let atHome = 0;
+  
+  for(let i = 0; i < weather.length; i++) {
+    if((i % 2 === 0) && (weather[i] === "rainy" || weather[i] === "thunderstorms")) {
+      atWork++;
+      atHome > 0 ? atHome-- : umbrellas++;
+    } else if ((i % 2 !== 0) && (weather[i] === "rainy" || weather[i] === "thunderstorms")) {
+      atHome++;
+      atWork > 0 ? atWork-- : umbrellas++
+    }
   }
+  return umbrellas
+}
