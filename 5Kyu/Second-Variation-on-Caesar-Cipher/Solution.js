65e08fafbc8fc5b5bba6df55a9a703a93fff9fa9
cgarbs <christian.garba@gmail.com>
2023-01-22 16:24:35 -0600

    done

--- 5Kyu/Second-Variation-on-Caesar-Cipher/Solution.js
+++ 5Kyu/Second-Variation-on-Caesar-Cipher/Solution.js
@@ -1,43 +1,25 @@
-//WORK IN PROGRESS.
-
-// function encodeStr(s, shift) {
-//     let first = String.fromCharCode(s.charCodeAt(s[0]) + shift).toLowerCase()
-//     let prefix = s[0].toLowerCase().concat(first)
-//     let ascii = s.split('').map(x => x.charCodeAt(x) >= 65 && x.charCodeAt(x) <= 90 ? x.charCodeAt(x) + 1 : 
-//                                       x.charCodeAt(x) >= 97 && x.charCodeAt(x) <= 122 ? x.charCodeAt(x) + 1 :
-//                                       x.charCodeAt(x))
-//     let encoded = ascii.map(x => String.fromCharCode(x))
-//     let result = prefix.concat(encoded.join(''))
-//     let x = Math.ceil(result.length / 5)
-//     return result.match(new RegExp('.{1,' + x + '}', 'g'));
-//   }  
-  
-//   function decode(arr) {
-//       // your code
-//   }
-
-
 const encodeStr = (s, shift) => {
   const rotate = s[0].toLowerCase() + String.fromCharCode(s[0].toLowerCase().charCodeAt(0) + shift)
-  const encoded = s.split('').map((e) => e.charCodeAt(0)).map((e) => e < 48 ? String.fromCharCode(e) : String.fromCharCode(e + shift))
-  
-    encoded.unshift(rotate[1])
-    encoded.unshift(rotate[0])
-    console.log(s.length)
-    console.log(rotate)
-    console.log(encoded)
-    
+  const encoded = s.split('').map((e) => e.charCodeAt(0)).map((e) => e < 64 ? String.fromCharCode(e) : String.fromCharCode(e + shift))
+
+  encoded.unshift(rotate[1])
+  encoded.unshift(rotate[0])
+
   const chunkSize = Math.ceil(encoded.length / 5);
   const newArray = []
-    
+
   for(let i = 0; i < encoded.length; i += chunkSize) {
     let chunk = encoded.slice(i, i + chunkSize);
     newArray.push(chunk.join(''))
   }
-    console.log(newArray)
     return newArray
-  }
-  
-  function decode(arr) {
-      console.log(arr)
-  }
+}
+
+function decode(arr) {
+  const array = arr.join('').split('')
+  const prefix = array.shift() + array.shift()
+  const shift = prefix.charCodeAt(1) - prefix.charCodeAt(0)
+
+  const decoded = array.map((e) => e.charCodeAt(0)).map((e) => e < 64 ? String.fromCharCode(e) : String.fromCharCode(e - shift)).join('');
+  return decoded
+}
