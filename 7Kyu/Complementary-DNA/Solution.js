7540063ee85c71fe3aa9d36056e454e1435ae5a8
cgarbs <christian.garba@gmail.com>
2023-01-26 05:44:56 -0600

    done

---
+++ 7Kyu/Complementary-DNA/Solution.js
@@ -0,0 +1,9 @@
+const DNAStrand = (dna) => dna.split('').map((e) => e === "A" ? "T"
+                                            : e === "T" ? "A"
+                                            : e === "C" ? "G"
+                                            : "C").join('');
+
+//OR
+
+const DNAStrand = (dna) => dna.split('').map(function(v) {return {A:'T', T:'A', C:'G', G:'C'}[v];}).join('');
+                                              
