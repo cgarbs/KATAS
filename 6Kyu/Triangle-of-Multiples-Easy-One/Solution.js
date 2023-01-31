6c178abe832b7a02ac101920a193fb698d97e9d6
cgarbs <christian.garba@gmail.com>
2023-01-31 09:21:58 -0600

    done

--- 6Kyu/Triangle-of-Multiples-Easy-One/Solution.js
+++ 6Kyu/Triangle-of-Multiples-Easy-One/Solution.js
@@ -1,20 +1,23 @@
 const multTriangle = (n) => {
-  let multiples = []
   let array = []
   
   for(let i = 1; i <= n; i++) {
     for(let j = 1; j <= i; j++) {
-      j === i ? multiples.push(i * j) : multiples.push(i * j) && multiples.push(i * j)
+      j === i ? array.push(i * j) : array.push(i * j) && array.push(i * j)
     } 
-    array.push(multiples)
-    multiples = []
   }
-  const result = []
-  result[0] = array.flat().reduce((a, b) => a + b, 0)
-  result[1] = array.flat().filter((e) => e % 2 === 0).reduce((a, b) => a + b, 0)
-  result[2] = array.flat().filter((e) => e % 2 !== 0).reduce((a, b) => a + b, 0)
-
-  return result
+  
+  return [
+    array.reduce((a, b) => a + b, 0),
+    array.reduce((a, b) => b % 2 === 0 ? a + b : a, 0),
+    array.reduce((a, b) => b % 2 !== 0 ? a + b : a, 0)
+  ]
 }
 
-// Code works, but not inside codewars due to flat().
+// OR
+
+const multTriangle = (n) => {
+  const totalSum = (n * (n + 1) / 2) ** 2
+  const oddSum = Math.ceil(n / 2) ** 4;
+  return [totalSum, totalSum - oddSum, oddSum];
+}
